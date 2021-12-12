09acee7ac8faa30de3ddef6ac1cf4a0fa49e35a7
Anna <anyakor99@gmail.com>
2021-12-13 02:13:50 +0300

    add info message in install.py

--- unix_commands/install.py
+++ unix_commands/install.py
@@ -2,10 +2,11 @@
 import os
 import shutil
 import stat
+import sys
 
 
 dest_folder = os.environ['PATH'].split(os.pathsep)[0]
-sys.stdout.write(f'The destination folder in PATH is {dest_folder}')
+sys.stdout.write(f'The destination folder in PATH is {dest_folder}\n')
 utils_list = ['ls.py', 'rm.py', 'sort.py', 'wc.py', 'grep.py', 'cat.py', 'tail.py', 'mkdir.py']
 
 for i in utils_list:
@@ -13,5 +14,4 @@ for i in utils_list:
     source = os.path.abspath(i)
     os.chmod(source, stat.S_IRWXU)
     new_path = shutil.copyfile(source, dest)
-    sys.stdout.write(f'The {source} has copied to {new_path}')
     os.chmod(new_path, stat.S_IRWXU)
