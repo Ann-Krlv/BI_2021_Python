1eba38abd76b6be8d3657e14e9985329e3867555
Anna Korolyova <90977030+Ann-Krlv@users.noreply.github.com>
2021-12-13 02:09:45 +0300

    Update install.py

--- unix_commands/install.py
+++ unix_commands/install.py
@@ -5,10 +5,13 @@ import stat
 
 
 dest_folder = os.environ['PATH'].split(os.pathsep)[0]
+sys.stdout.write(f'The destination folder in PATH is {dest_folder}')
 utils_list = ['ls.py', 'rm.py', 'sort.py', 'wc.py', 'grep.py', 'cat.py', 'tail.py', 'mkdir.py']
+
 for i in utils_list:
     dest = os.path.join(dest_folder, i)
     source = os.path.abspath(i)
     os.chmod(source, stat.S_IRWXU)
     new_path = shutil.copyfile(source, dest)
+    sys.stdout.write(f'The {source} has copied to {new_path}')
     os.chmod(new_path, stat.S_IRWXU)
